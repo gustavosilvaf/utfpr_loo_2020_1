85d3031190501110657797210915d59b8fefa2a2
Gustavo Silva <gustavofsilva@email.com>
2020-03-10 10:19:49 -0300

    03-ex02 ending the exercise

--- 03-ex02-heater-exercise/src/com/gustavo/Heater.java
+++ 03-ex02-heater-exercise/src/com/gustavo/Heater.java
@@ -1,9 +1,56 @@
 package com.gustavo;
 
 public class Heater {
+    private int temperature, min, max, increment;
+
+    /**
+     *
+     * @param minTemperature is the minimum temperature
+     * @param maxTemperature is the maximum temperature
+     */
+    public Heater(int minTemperature, int maxTemperature) {
+        temperature = 15;
+        min = minTemperature;
+        max = maxTemperature;
+        increment = 5;
+    }
 
 
-    public Heater() {
+    /**
+     * when called, increases the temperature by the increment value if it does not exceed the maximum
+     */
+    public void warmUp(){
+        if ((temperature + increment) > max)
+            System.err.println("Value exceeds the maximum");
 
+        else
+            temperature += increment;
+    }
+
+    /**
+     * when called, decreases the temperature by the increment value if it does not exceed the minimum
+     */
+    public void cool(){
+        if ((temperature - increment) < min)
+            System.err.println("Value exceeds the minimum");
+        else
+            temperature -= increment;
+    }
+
+    /**
+     *
+     * @return the temperature
+     */
+    public int getTemperature() {
+        return temperature;
+    }
+
+    /**
+     *
+     * @param value is the new increment value
+     */
+
+    public void setIncrement(int value){
+        increment = value;
     }
 }
--- 03-ex02-heater-exercise/src/com/gustavo/Main.java
+++ 03-ex02-heater-exercise/src/com/gustavo/Main.java
@@ -3,6 +3,33 @@ package com.gustavo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Heater h = new Heater(3, 18);
+        h.setIncrement(3);
+        h.cool();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.cool();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.cool();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.cool();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.cool();//no effect
+        System.out.println("Temperature:"+h.getTemperature());
+        h.cool();//no effect
+        System.out.println("Temperature:"+h.getTemperature());
+        h.warmUp();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.warmUp();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.warmUp();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.warmUp();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.warmUp();
+        System.out.println("Temperature:"+h.getTemperature());
+        h.warmUp();//no effect
+        System.out.println("Temperature:"+h.getTemperature());
+        h.warmUp();//no effect
+        System.out.println("Temperature:"+h.getTemperature());
     }
 }
